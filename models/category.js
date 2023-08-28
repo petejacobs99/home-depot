5c2ac1790509de279ef432087cc8916f66e5af20
mimo217 <127803556+mimo217@users.noreply.github.com>
2023-08-28 16:05:51 -0500

    created department category wishlist models (#12)
    Co-authored-by: Jillian Guerra <128921208+jillianguerra@users.noreply.github.com>

--- models/category.js
+++ models/category.js
@@ -1,1 +1,10 @@
-/*test*/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const categorySchema = new Schema({
+    name: { type: String, required: true },
+    image: {type: String},
+    department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true }
+})
+
+module.exports = mongoose.model('Category', categorySchema);
---
+++ models/wishlist.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const wishlistSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product'}]
+})
+
+module.exports = mongoose.model('Wishlist', wishlistSchema);

1e7e09b61b5ec12a8d658b3301778222db1a7350
mel b <114832865+lovewitchwifi@users.noreply.github.com>
2023-08-29 02:23:52 +0300

    Create a new branch (#20)
    * test speed drive
    
    * user model
    
    ---------
    
    Co-authored-by: Jillian Guerra <128921208+jillianguerra@users.noreply.github.com>

--- models/user.js
+++ models/user.js
@@ -1,1 +1,72 @@
-// mel is responsible for this user model eep
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const SALT_ROUNDS = 10;
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  shippingAddress: {
+    addressLine1: String,
+    addressLine2: String,
+    city: String,
+    state: String,
+    postalCode: String,
+    country: String
+  },
+  orderHistory: [
+    {
+      orderId: String,
+      date: Date,
+      totalAmount: Number,
+      items: [
+        {
+          productId: String,
+          productName: String,
+          quantity: Number,
+          price: Number
+        }
+      ]
+    }
+  ],
+  savedItems: [
+    {
+      productId: String,
+      productName: String,
+      price: Number,
+      imageUrl: String
+    }
+  ]
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: function(doc, ret) {
+      delete ret.password;
+      return ret;
+    }
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);

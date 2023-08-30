ef6fb0c80c273e6c46b95c15bb11b2e1436f96bf
lcohen730 <lcohen730@gmail.com>
2023-08-29 22:27:33 -0400

    Add Styles and Functionality to Hamburger Menu

--- src/components/HamMenu/HamMenu.js
+++ src/components/HamMenu/HamMenu.js
@@ -1,29 +1,51 @@
-export default function HamMenu() {
+import { getUser } from '../../utilities/users-service';
 
+export default function HamMenu() {
+    const user = getUser();
     const handleClick = () => {
         console.log('Clicked!')
     }
 
     return (
-        <ul>
-            <li>
-                { user ? 
-                    <ul>
-                        <li onClick={handleClick}>MY ACCOUNT</li>
-                        <li onClick={handleClick}>WISHLIST</li>
-                        <li onClick={handleClick}>ORDER HISTORY</li>
-                        <li onClick={handleClick}>LOGOUT</li>
-                    </ul>
-                    :
-                    <p>SIGN IN / SIGN UP</p>
-                }
-            </li>
-            <li>
-                <ul>
-                    <li onClick={handleClick}>FAQ</li>
-                    <li onClick={handleClick}>ABOUT US</li>
-                </ul>
-            </li>
+        <ul className="menuContainer">
+          <li>
+            {user ? (
+              <ul className="userOptions">
+                <li
+                  onClick={handleClick}
+                >
+                  MY ACCOUNT
+                </li>
+                {/* <Link to="/users" >MY ACCOUNT</Link> */}
+                <li onClick={handleClick}>
+                  WISHLIST
+                </li>
+                {/* <Link to="/wishlist" >WISHLIST</Link> */}
+                <li
+                  onClick={handleClick}
+                >
+                  ORDER HISTORY
+                </li>
+                {/* <Link to="/orders" >ORDER HISTORY</Link> */}
+                <li onClick={handleClick}>LOGOUT</li>
+              </ul>
+            ) : (
+              <p className="auth" onClick={handleClick}>
+                SIGN IN / SIGN UP
+              </p>
+            )}
+          </li>
+          {/* <p>{result}</p> */}
+          <li>
+            <ul className="info">
+              <li onClick={handleClick}>
+                FAQ
+              </li>
+              <li onClick={handleClick}>
+                ABOUT US
+              </li>
+            </ul>
+          </li>
         </ul>
-    );
-}
+      );
+    }

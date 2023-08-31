c68d1f8047df8dd4c4371792c34231e4ed909283
misselysea <elyseacrawford@gmail.com>
2023-08-31 15:49:49 -0400

    Updated the Review component

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,14 +1,24 @@
 import DynamicStars from './DynamicStars';
 import styles from './Review.module.scss';
 
-const Review = ({ review }) => {
+const Review = ({ review, userReview }) => {
   return (
     <div className={styles.review}>
       <h3>{review.reviewerName}</h3>
       <p>{review.reviewText}</p>
       <DynamicStars rating={review.rating} />
+
+    
+      {userReview && (
+        <div className={styles.userReview}>
+          <h4>Your Review:</h4>
+          <p>{userReview.reviewText}</p>
+          <DynamicStars rating={userReview.rating} />
+        </div>
+      )}
     </div>
   );
 };
 
 export default Review;
+

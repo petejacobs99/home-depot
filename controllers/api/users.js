2e3eed1b2252dafa7086b6a078b933f6fb930abd
username <Jzazzi56@gmail.comn>
2023-09-07 16:38:09 -0700

    made update controller for user and changed isguest to false

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,30 +1,30 @@
-const User = require('../../models/user')
-const bcrypt = require('bcrypt')
+const User = require('../../models/user');
+const bcrypt = require('bcrypt');
 
 const checkToken = (req, res) => {
-  console.log('req.user', req.user)
-  res.json(req.exp)
-}
+	console.log('req.user', req.user);
+	res.json(req.exp);
+};
 
 const dataController = {
-  async create(req, res, next) {
-    try {
-      const user = await User.create(req.body)
-      const isGuest = req.body.isGuest
-      // token will be a string
-      const token = await user.createJWT()
-      // send back the token as a string
-      // which we need to account for
-      // in the client
-      res.locals.data.user = user
-      res.locals.data.token = token
-      next()
-    } catch (e) {
-      console.log('you got a database problem')
-      res.status(400).json(e)
-    }
-  },
-  //update put request findbyIdUpdate
+	async create(req, res, next) {
+		try {
+			const user = await User.create(req.body);
+			const isGuest = req.body.isGuest;
+			// token will be a string
+			const token = await user.createJWT();
+			// send back the token as a string
+			// which we need to account for
+			// in the client
+			res.locals.data.user = user;
+			res.locals.data.token = token;
+			next();
+		} catch (e) {
+			console.log('you got a database problem');
+			res.status(400).json(e);
+		}
+	},
+	//update put request findbyIdUpdate
 	async update(req, res, next) {
 		try {
 			const user = await User.findByIdAndUpdate(req.params.id, req.body, {
@@ -36,31 +36,42 @@ const dataController = {
 			res.status(400).json({ message: e.message });
 		}
 	},
-  
-  async login(req, res, next) {
-    try {
-      const user = await User.findOne({ email: req.body.email })
-      if (!user) throw new Error()
-      const match = await bcrypt.compare(req.body.password, user.password)
-      if (!match) throw new Error()
-      res.locals.data.user = user
-      res.locals.data.token = await user.createJWT()
-      next()
-    } catch {
-      res.status(400).json('Bad Credentials')
-    }
-  }
 
-}
+	async login(req, res, next) {
+		try {
+			const user = await User.findOne({ email: req.body.email });
+			if (!user) throw new Error();
+			const match = await bcrypt.compare(req.body.password, user.password);
+			if (!match) throw new Error();
+			res.locals.data.user = user;
+			res.locals.data.token = await user.createJWT();
+			next();
+		} catch {
+			res.status(400).json('Bad Credentials');
+		}
+	},
+	//update put request findbyIdUpdate
+	async update(req, res, next) {
+		try {
+			const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+				new: true
+			});
+			res.locals.data.user = user;
+			next();
+		} catch (e) {
+			res.status(400).json({ message: e.message });
+		}
+	}
+};
 
 const apiController = {
-  auth(req, res) {
-    res.json(res.locals.data.token)
-  }
-}
+	auth(req, res) {
+		res.json(res.locals.data.token);
+	}
+};
 
 module.exports = {
-  checkToken,
-  dataController,
-  apiController
-}
+	checkToken,
+	dataController,
+	apiController
+};
--- models/user.js
+++ models/user.js
@@ -1,40 +1,45 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const Schema = mongoose.Schema
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Schema = mongoose.Schema;
 
-const SALT_ROUNDS = 10
+const SALT_ROUNDS = 10;
 
-const userSchema = new Schema({
-  firstName: { type: String, required: true, default: 'Guest'},
-  lastName: String,
-  email: { type: String, unique: true, lowercase: true },
-  password: String,
-  isGuest: { type: Boolean, default: true }
-}, {
-  timestamps: true,
-  toJSON: {
-    transform: function(doc, ret) {
-      delete ret.password
-      return ret
-    }
-  }
-})
+const userSchema = new Schema(
+	{
+		firstName: { type: String, required: true },
+		lastName: String,
+		email: { type: String, unique: true, lowercase: true },
+		password: String,
+		isGuest: { type: Boolean, default: false }
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			transform: function (doc, ret) {
+				delete ret.password;
+				return ret;
+			}
+		}
+	}
+);
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next()
-  this.password = await bcrypt.hash(this.password, SALT_ROUNDS)
-  return next()
-})
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) return next();
+	this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+	return next();
+});
 
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password)
-}
-userSchema.methods.createJWT = async function() {
-    const token = jwt.sign({_id: this._id}, process.env.SECRET, {expiresIn: '24h'})
-    // creates a token using json web token, the token expires in 24 hours, the secret comes from the .env file
-    return token
-}
+userSchema.methods.comparePassword = async function (candidatePassword) {
+	return await bcrypt.compare(candidatePassword, this.password);
+};
+userSchema.methods.createJWT = async function () {
+	const token = jwt.sign({ _id: this._id }, process.env.SECRET, {
+		expiresIn: '24h'
+	});
+	// creates a token using json web token, the token expires in 24 hours, the secret comes from the .env file
+	return token;
+};
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);

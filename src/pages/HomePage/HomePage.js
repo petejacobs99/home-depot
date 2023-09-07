d75738708759d25d13d07c6a5a7c629615b2ad94
sanlaelaecho <sanlaelaecho@gmail.com>
2023-09-06 19:18:51 -0500

    home page

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,31 @@
+import styles from './HomePage.module.scss';
+import { useState, useEffect, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import FeaturedItemCarousel from './components/FeaturedItemCarousel/FeaturedItemCarousel';
+import DepartmentList from './components/DepartmentList/DepartmentList';
+
+export default function App({ user, setUser }) {
+	const [depList, setDepList] = useState([]);
+	const departmentsRef = useRef([]);
+	const navigate = useNavigate();
+
+	useEffect(function () {
+		async function getDeps() {
+			//const deps = await depAPI.getAllDepartments()
+		}
+		//setDepList(deps)
+	}, []);
+
+	return (
+		<div className={styles.HomePage}>
+			<div className={styles.NavBar}>
+				<NavBar />
+			</div>
+			<FeaturedItemCarousel className={styles.Carousel} />
+			<div className="categoryList">
+				<CategoryList />
+			</div>
+		</div>
+	);
+}

47b95392694bb774bc89f43a1ea0e2739b95b840
lcohen730 <lcohen730@gmail.com>
2023-09-09 13:49:19 -0400

    Add Placeholder Images for Category List and Fix Styling

--- src/components/CatListItem/CatListItem.js
+++ src/components/CatListItem/CatListItem.js
@@ -1,6 +1,6 @@
 import styles from "./CatListItem.module.scss";
-import * as catsAPI from '../../utilities/catDep-api';
-import * as itemsAPI from '../../utilities/items-api';
+/* import * as catsAPI from '../../utilities/catDep-api';
+import * as itemsAPI from '../../utilities/items-api'; */
 /* import { useNavigate } from 'react-router-dom'; */
 
 
@@ -14,7 +14,11 @@ export default function CatListItem({ categories, category, handleClick }) {
 
     return (
         <div className={styles.catListItem} onClick={handleClick}>
-            <img className={styles.image} src={category.image} />
+            {/* <img className={styles.image} src={category.image} /> */}
+            <img 
+                className={styles.image} 
+                src="https://images.thdstatic.com/productImages/1d64ea68-9f7f-45d3-ac5e-20b7c8522141/svn/orange-the-home-depot-paint-buckets-05glhd2-64_1000.jpg" 
+            />
             <div className={styles.name}>{category.name}</div>
         </div>
     )

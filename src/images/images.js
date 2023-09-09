61c2095dbdf1450f0659174353a6fab7d35e36f2
username <Jzazzi56@gmail.comn>
2023-09-09 14:27:46 -0700

    updated assets folder and images folder

--- src/images/images.js
+++ src/images/images.js
@@ -23,8 +23,68 @@ import kitchenBG4 from '../assets/kitchenBG4.jpeg';
 import dewaltDrill from '../assets/dewalt.webp';
 import craftsmanDrill from '../assets/craftsman.webp';
 import rockwellDrill from '../assets/rockwell.jpeg';
-//Tools Background
+//Backgrounds
 import toolsBG from '../assets/toolsBG.jpeg';
+import fridgeBG from '../assets/fridgeBG.jpeg';
+import ovenBG from '../assets/ovenBG/jpeg';
+import washerdryerBG from '../assets/washerdryerBG.jpeg';
+import microwaveBG from '../assets/microwave.jpeg';
+import drillBG from '../assets/drillBG.jpeg';
+//cabinets
+import HBcabinet from '../assets/Hampton-Bay.png';
+import HDCcabinet from '../assets/Home-Decorators-Collection.png';
+import CECcabinet from '../assets/Contractor-Express-Cabinets.png';
+//cabinet background
+import cabinetsBG from '../assets/cabinetsBG.png';
+//kitcheb faucets
+import faucetBG from '../assetsfaucetBGjpg';
+import glacierBay from '../assets/Glacier-Bay.png';
+import kohler from '../assets/KOHLER.png';
+import delta from '../assets/Delta.png';
+//backsplash
+import backSplashBG from '../assets/backsplashBG.png';
+import MSI from '../assets/MSI.png';
+import Daltile from '../assets/Daltile.png';
+import Marazzi from '../assets/Marazzi.png';
+//Garbage disposal
+import gdBG from '../assets/garbagedisosalBG.jpeg';
+import inSinkErator from '../assets/InSinkErator.png';
+import MOEN from '../assets/MOEN.png';
+import wasteKing from '../assets/Waste-King.png';
+//tubs
+import tubBG from '../assets/tubBG.jpeg';
+import vanityArt from '../assets/Vanity-Art.png';
+import HDCtub from '../assets/Home-Decorators-Collection-tub.png';
+import kohlerTub from '../assets/KOHLER-tub.png';
+//Shower heads
+import showerHeadBG from '../assets/showerHeadBG.jpeg';
+import deltaSH from '../assets/Delta-SH.png';
+import MOENSH from '../assets/MOEN-SH.png';
+import glacierBaySH from '../assets/Glacier-Bay-SH.png';
+//bathroom faucets
+import bfBG from '../assets/bfBG.jpeg';
+import glacierBayBF from '../assets/Glacier-Bay-BF.png';
+import pfisterBF from '../assets/Pfister-BF.png';
+import deltaBF from '../assets/Delta-BF.png';
+//toilets
+import toiletBG from '../assets/toiletsBG.webp';
+import glacierBayToilet from '../assets/Glacier-Bay-toliet.png';
+import americanStandardToilet from '../assets/American-Standard-toilet.png';
+import KohlerToilet from '../assets/KOHLER-toilet.png';
+//Toolset
+import stanleyTS from '../assets/StanleyTS.png';
+import dewaltTS from '../assets/DEWALTTS.png';
+import kingTS from '../assets/KINGTS.png';
+//Nuts and Bolts
+import starkNB from '../assets/StarkNB.png';
+import everBitNB from '../assets/EverbitNB.png';
+import tradesProNB from '../assets/TradesProNB.png';
+import NBBG from '../assets/nutsandboltsBG.jpeg';
+//buckets
+import redBucket from '../assets/bucket1.jpeg';
+import blackBucket from '../assets/blackbucket.jpeg';
+import whiteBucketSet from '../assets/whitebucketset.jpeg';
+import bucketsBG from '../assets/bucketsBG.jpeg';
 
 export default {
 	lgFridge,
@@ -52,5 +112,65 @@ export default {
 	craftsmanDrill,
 	rockwellDrill,
 
-	toolsBG
+	toolsBG,
+
+	HBcabinet,
+	HDCcabinet,
+	CECcabinet,
+	cabinetsBG,
+
+	faucetBG,
+	glacierBay,
+	kohler,
+	delta,
+
+	backSplashBG,
+	MSI,
+	Daltile,
+	Marazzi,
+
+	gdBG,
+	inSinkErator,
+	MOEN,
+	wasteKing,
+
+	tubBG,
+	kohlerTub,
+	vanityArt,
+	HDCtub,
+
+	showerHeadBG,
+	deltaSH,
+	MOENSH,
+	glacierBaySH,
+
+	bfBG,
+	glacierBayBF,
+	deltaBF,
+	pfisterBF,
+
+	toiletBG,
+	glacierBayToilet,
+	americanStandardToilet,
+	KohlerToilet,
+
+	stanleyTS,
+	dewaltTS,
+	kingTS,
+
+	starkNB,
+	everBitNB,
+	tradesProNB,
+	NBBG,
+
+	whiteBucketSet,
+	redBucket,
+	blackBucket,
+	bucketsBG,
+
+	fridgeBG,
+	ovenBG,
+	washerdryerBG,
+	microwaveBG,
+	drillBG
 };

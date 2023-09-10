7a520794c7aa49d997512130fc652a22efbe8599
misselysea <elyseacrawford@gmail.com>
2023-09-10 17:20:55 -0400

    Order Page

--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -1,19 +1,17 @@
 import styles from './OrderPage.module.scss';
+import CartDetail from './CartDetail';
 
 export default function OrderPage({ cart, handleChangeQty, handleCheckout }) {
   return (
     <div className={styles.container}>
       <h1>Your Cart</h1>
-      {Object.keys(cart).map((itemId) => (
-        <div key={itemId} className={styles.form}>
-          <span>Item: {cart[itemId].name}</span>
-          <span>Quantity: {cart[itemId].quantity}</span>
-          <button className={styles.editBtn} onClick={() => handleChangeQty(itemId, cart[itemId].quantity + 1)}>+</button>
-          <button className={styles.editBtn} onClick={() => handleChangeQty(itemId, cart[itemId].quantity - 1)}>-</button>
-        </div>
-      ))}
-      <button className={styles.editBtn} onClick={handleCheckout}>Proceed to Checkout</button>
+      <CartDetail
+        cart={cart}
+        handleChangeQty={handleChangeQty}
+        handleCheckout={handleCheckout}
+      />
     </div>
   );
 }
+
 

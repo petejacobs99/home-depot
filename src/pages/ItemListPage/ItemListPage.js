2448fbd2d7d41f6ccc3aa89b227425152787064a
lcohen730 <lcohen730@gmail.com>
2023-09-12 17:14:21 -0400

    Items By Category are Rendering Properly Nowgit add -A

--- src/pages/ItemListPage/ItemListPage.js
+++ src/pages/ItemListPage/ItemListPage.js
@@ -5,30 +5,32 @@ import { useNavigate, useParams } from 'react-router-dom';
 import * as itemsAPI from "../../utilities/items-api";
 import ItemList from "../../components/ItemList/ItemList";
 
-export default function ItemListPage() {
+export default function ItemListPage({ handleAddToOrder }) {
   const [itemListItems, setItemListItems] = useState([]);
-  /* const [activeCat, setActiveCat] = useState(''); */
-  /* const categoriesRef = useRef([]); */
+  const [activeCat, setActiveCat] = useState('');
+  const categoriesRef = useRef([]);
   const navigate = useNavigate();
   const params = useParams();
 
   useEffect(function () {
     async function getItems() {
-      /* const items = await itemsAPI.getAll();
-      categoriesRef.current = items.reduce((cats, item) => {
+      const items = await itemsAPI.getAll();
+      categoriesRef.current = items.items.reduce((cats, item) => {
         const cat = item.category.name;
         return cats.includes(cat) ? cats : [...cats, cat];
-      }, []); */
-      const items = await itemsAPI.getByCategory(params.catName);
-      setItemListItems(items);
+      }, []);
+      /* const items = await itemsAPI.getByCategory(params.catName); */
+      setItemListItems(items.items);
       /* setActiveCat(categoriesRef.current[0]); */
+      const actCatName = categoriesRef.current.find((cat) => cat === params.catName);
+      setActiveCat(actCatName);
     }
     getItems();
   }, []);
 
-  async function handleClick(i) {
+ /*  async function handleClick(i) {
     navigate(`/home/categories/items/${itemListItems[i]._id}`);
-  }
+  } */
 
   return (
     <div className={styles.App}>
@@ -36,7 +38,7 @@ export default function ItemListPage() {
         <ItemList 
           itemListItems={itemListItems.filter(item => item.category.name === activeCat)}
           setItemListItems={setItemListItems} 
-          onClick={handleClick}
+          onClick={handleAddToOrder}
         />
       </div>
     </div>

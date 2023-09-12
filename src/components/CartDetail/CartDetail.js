445c70006504747fe83ce4951e4f084fcad7e0e4
lcohen730 <lcohen730@gmail.com>
2023-09-12 14:21:46 -0400

    Add Cart Ternary so Something Shows Up in the Cart Detail Component Even with No Items in Cart and Minor Style Changes

--- src/components/CartDetail/CartDetail.js
+++ src/components/CartDetail/CartDetail.js
@@ -2,17 +2,23 @@ import styles from './CartDetail.module.scss';
 
 export default function CartDetail({ cart, handleChangeQty, handleCheckout }) {
   return (
-    <div className={styles.container}>
-      <h1>Your Cart</h1>
-      {Object.keys(cart).map((itemId) => (
-        <div key={itemId} className={styles.item}>
-          <span>Item: {cart[itemId].name}</span>
-          <span>Quantity: {cart[itemId].quantity}</span>
-          <button onClick={() => handleChangeQty(itemId, cart[itemId].quantity + 1)}>+</button>
-          <button onClick={() => handleChangeQty(itemId, cart[itemId].quantity - 1)}>-</button>
+    <>
+      {cart ?
+        <div className={styles.container}>
+          <h1>YOUR CART</h1>
+          {Object.keys(cart).map((itemId) => (
+            <div key={itemId} className={styles.item}>
+              <span>Item: {cart[itemId].name}</span>
+              <span>Quantity: {cart[itemId].quantity}</span>
+              <button onClick={() => handleChangeQty(itemId, cart[itemId].quantity + 1)}>+</button>
+              <button onClick={() => handleChangeQty(itemId, cart[itemId].quantity - 1)}>-</button>
+            </div>
+          ))}
+          <button onClick={handleCheckout} className={styles.checkoutBtn}>Proceed to Checkout</button>
         </div>
-      ))}
-      <button onClick={handleCheckout} className={styles.checkoutBtn}>Proceed to Checkout</button>
-    </div>
+        :
+        <p>YOUR CART IS EMPTY.</p>
+    }
+    </>
   );
 }

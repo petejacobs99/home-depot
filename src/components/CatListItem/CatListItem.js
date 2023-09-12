24c928f0c0a371e5cc6a53c940cc7a588a4158e6
lcohen730 <lcohen730@gmail.com>
2023-09-12 14:26:12 -0400

    Category List Items Have the Seed Pictures and Link to the Correct Item Page When Clicked and Minor Style Changes

--- src/components/CatListItem/CatListItem.js
+++ src/components/CatListItem/CatListItem.js
@@ -17,10 +17,11 @@ export default function CatListItem({ categories, category, handleClick }) {
         <div className={styles.catListItem} /* onClick={handleClick} */>
             {/* <img className={styles.image} src={category.image} /> */}
             <Link to={`/home/${params.depName}/${category.name}/items`}>
-                <img
+                {/* <img
                     className={styles.image}
                     src="https://images.thdstatic.com/productImages/1d64ea68-9f7f-45d3-ac5e-20b7c8522141/svn/orange-the-home-depot-paint-buckets-05glhd2-64_1000.jpg"
-                />
+                /> */}
+                <img className={styles.image} src={category.image} />
                 <div className={styles.name}>{category.name}</div>
             </Link>
         </div>

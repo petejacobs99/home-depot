6a5005e7744119d3322c203463e44d706cb08dd6
lcohen730 <lcohen730@gmail.com>
2023-09-12 16:53:14 -0400

    Just Cleaning Up Code

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -8,35 +8,27 @@ export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);
 
   return (
-    <main className={styles.AuthPage}>
-      <div className={styles.container}>
-        {/* <div> */}
-        {/* <h3
-            className={showLogin ? styles.signUpText : styles.logInText}
-            onClick={() => setShowLogin(!showLogin)}
-          >
-            {showLogin ? 'Create an Account' : 'Sign In'}
-          </h3> */}
-        <h3>
-          {showLogin ? 'SIGN IN' : 'CREATE AN ACCOUNT'}
-        </h3>
-        {/* </div> */}
-        {showLogin ? (
-          <div>
-            <LoginForm setUser={setUser} />
-            <p>DON'T HAVE AN ACCOUNT?</p>
-            <button onClick={() => setShowLogin(!showLogin)}>CREATE AN ACCOUNT</button>
-          </div>
-        ) : (
-          <div>
-            <SignUpForm setUser={setUser} />
-            <button className={styles.back} onClick={() => setShowLogin(!showLogin)}>← SIGN IN</button>
-          </div>
-        )}
-      </div>
-      <Link to="/home/categories" className={styles.back}>RETURN TO HOME PAGE</Link>
-    </main>
+    <>
+      <main className={styles.AuthPage}>
+        <div className={styles.container}>
+          <h3>
+            {showLogin ? 'SIGN IN' : 'CREATE AN ACCOUNT'}
+          </h3>
+          {showLogin ? (
+            <div>
+              <LoginForm setUser={setUser} />
+              <p>DON'T HAVE AN ACCOUNT?</p>
+              <button onClick={() => setShowLogin(!showLogin)}>CREATE AN ACCOUNT</button>
+            </div>
+          ) : (
+            <div>
+              <SignUpForm setUser={setUser} />
+              <button className={styles.back} onClick={() => setShowLogin(!showLogin)}>← SIGN IN</button>
+            </div>
+          )}
+        </div>
+        {/* <Link to="/home/categories" className={styles.back}>RETURN TO HOME PAGE</Link> */}
+      </main>
+    </>
   );
 }
-
-// need to implement guest login

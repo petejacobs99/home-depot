d2f8731b55caf693f5a0c63e2baf53d4dca150f5
lcohen730 <lcohen730@gmail.com>
2023-09-13 15:47:19 -0400

    Update Search Results Page to Work with New Search Results and Search Results Components and Minor Styling Changes

--- src/pages/SearchResultsPage/SearchResultsPage.js
+++ src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,33 +1,30 @@
-import React, { useState } from "react";
-import ItemList from "../../components/ItemList/ItemList";
-import SearchBar from "../../components/SearchBar/SearchBar"; // Import the SearchBar component
+import { useState } from "react";
 import styles from "./SearchResultsPage.module.scss";
-import {searchItems} from "../../utilities/items-api";
+import { searchItems } from "../../utilities/items-api";
+import * as itemsAPI from "../../utilities/items-api";
 import { useParams } from 'react-router-dom';
+import ItemListItem from '../../components/ItemListItem/ItemListItem';
+import SearchResults from "../../components/SearchResults/SearchResults";
 
-export default function SearchResultsPage ({ handleAddToOrder, setItemListItems, itemListItems, updateSearchResults }) {
-  const [searchResults, setSearchResults] = useState([]); // State to store search results
-
-  // Function to update search results
-  /* const updateSearchResults = async (searchQuery) => {
-    const items = await searchItems(searchQuery);
-    setSearchResults(items);
-  }; */
-
+export default function SearchResultsPage({ handleAddToOrder, updateSearchResults }) {
+  const [searchResultsItems, setSearchResultsItems] = useState([]); // State to store search results
   const params = useParams();
 
+  // Function to update search results
   async function getSearchResults(searchQuery) {
-    const items = await searchItems(searchQuery);
-    setSearchResults(items);
+    const items = await itemsAPI.searchItems(searchQuery);
+    setSearchResultsItems(items.items);
   }
   getSearchResults(params.term);
+
+  /* const items = searchResults.map(item => {
+    <ItemListItem key={item._id} />
+  }) */
 
   return (
     <div>
       <div className={styles.searchResultsContainer}>
         <h1>SEARCH RESULTS FOR "{params.term}"</h1>
-        {/* Render the SearchBar component and pass the updateSearchResults function */}
-        {/* <SearchBar onSearch={updateSearchResults} />*/}
       </div>
       {/*<ItemList
         // Pass the searchResults state as the items to be displayed
@@ -37,7 +34,14 @@ export default function SearchResultsPage ({ handleAddToOrder, setItemListItems,
         setItemListItems={setItemListItems}
         itemListItems={itemListItems}
       /> */}
-      {searchResults}
+      <div className={styles.itemList}>
+        {/* {items} */}
+        <SearchResults 
+          searchResultsItems={searchResultsItems}
+          setSearchResultsItems={setSearchResultsItems} 
+          onClick={handleAddToOrder}
+        />
+      </div>
     </div>
   );
 };

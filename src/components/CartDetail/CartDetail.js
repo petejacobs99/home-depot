66f55db49139e208a186b9ca54446d0da67eba39
petejacobs99 <petejacobs99@gmail.com>
2023-09-13 17:06:31 -0400

    cart now uses lineitem component

--- src/components/CartDetail/CartDetail.js
+++ src/components/CartDetail/CartDetail.js
@@ -1,24 +1,43 @@
+import React, { useEffect, useState } from 'react';
 import styles from './CartDetail.module.scss';
+import { getCart } from '../../utilities/orders-api';
+import LineItem from '../LineItem/LineItem'; // Import the LineItem component
 
 export default function CartDetail({ cart, handleChangeQty, handleCheckout }) {
+  const [fetchedCart, setFetchedCart] = useState(null);
+
+  useEffect(() => {
+    const fetchCart = async () => {
+      try {
+        const fetchedCart = await getCart();
+        setFetchedCart(fetchedCart);
+      } catch (error) {
+        console.error('Error fetching cart:', error);
+      }
+    };
+
+    fetchCart();
+  }, []);
+
   return (
     <>
-      {cart ?
+      {cart || fetchedCart ? (
         <div className={styles.container}>
           <h1>YOUR CART</h1>
-          {Object.keys(cart).map((itemId) => (
-            <div key={itemId} className={styles.item}>
-              <span>Item: {cart[itemId].name}</span>
-              <span>Quantity: {cart[itemId].quantity}</span>
-              <button onClick={() => handleChangeQty(itemId, cart[itemId].quantity + 1)}>+</button>
-              <button onClick={() => handleChangeQty(itemId, cart[itemId].quantity - 1)}>-</button>
-            </div>
+          {Object.keys(cart || fetchedCart).map((itemId) => (
+            <LineItem
+              key={itemId}
+              item={cart ? cart[itemId] : fetchedCart[itemId]}
+              handleChangeQty={handleChangeQty}
+            />
           ))}
-          <button onClick={handleCheckout} className={styles.checkoutBtn}>Proceed to Checkout</button>
+          <button onClick={handleCheckout} className={styles.checkoutBtn}>
+            Proceed to Checkout
+          </button>
         </div>
-        :
+      ) : (
         <p>YOUR CART IS EMPTY.</p>
-    }
+      )}
     </>
   );
 }

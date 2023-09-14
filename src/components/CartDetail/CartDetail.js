b940e502a99746305736e29e61af63a2a6b7d02f
lcohen730 <lcohen730@gmail.com>
2023-09-14 14:23:05 -0400

    Add Handle Change Quantity Functionality to LineItem, Total Cart Value to Order Page, Other Changes to Make Order Page Render Properly, and Major Styling Updates

--- src/components/CartDetail/CartDetail.js
+++ src/components/CartDetail/CartDetail.js
@@ -3,43 +3,19 @@ import { getCart } from '../../utilities/orders-api';
 import { useState, useEffect } from "react";
 import LineItem from '../../components/LineItem/LineItem'; // Import the LineItem component
 
-export default function CartDetail({ cart, handleChangeQty, handleCheckout }) {
-  const [fetchedCart, setFetchedCart] = useState(null);
-
-  useEffect(() => {
-    const fetchCart = async () => {
-      /* try {
-        const fetchedCart = await getCart();
-        setFetchedCart(fetchedCart);
-      } catch (error) {
-        console.error('Error fetching cart:', error);
-      } */
-      const fetchedCart = await getCart();
-      setFetchedCart(fetchedCart);
-      console.log(fetchedCart)
-    };
-    fetchCart();
-  }, []);
+export default function CartDetail({ cart, handleChangeQty, fetchedCart, setFetchedCart, handleAddToWishList }) {
+  const items = fetchedCart.lineItems.map(item => 
+    <LineItem
+      key={item._id}
+      item={item}
+      handleChangeQty={handleChangeQty}
+      fetchedCart={fetchedCart}
+      setFetchedCart={setFetchedCart}
+      handleAddToWishList={handleAddToWishList}
+    />
+  )
 
   return (
-    <>
-      {cart || fetchedCart ? (
-        <div className={styles.container}>
-          {/* <h1>YOUR CART</h1> */}
-          {(cart || fetchedCart).lineItems.map((item) => (
-            <LineItem
-              key={item._id}
-              item={cart ? cart.lineItems[item] : fetchedCart.lineItems[item]}
-              handleChangeQty={handleChangeQty}
-            />
-          ))}
-          <button onClick={handleCheckout} className={styles.checkoutBtn}>
-            Proceed to Checkout
-          </button>
-        </div>
-      ) : (
-        <p>YOUR CART IS EMPTY.</p>
-      )}
-    </>
+    <div className={styles.lineItems}>{items}</div>
   );
 }
--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -1,15 +1,42 @@
 import styles from './OrderPage.module.scss';
 import CartDetail from '../../components/CartDetail/CartDetail';
+import { getCart } from '../../utilities/orders-api';
+import { useState, useEffect } from "react";
 
-export default function OrderPage({ cart, handleChangeQty, handleCheckout }) {
+export default function OrderPage({ cart, handleChangeQty, handleCheckout, handleAddToWishList }) {
+  const [fetchedCart, setFetchedCart] = useState(null);
+  /* const itemsAmount = fetchedCart.lineItems.length; */
+
+  useEffect(() => {
+    const fetchCart = async () => {
+      const fetchedCart = await getCart();
+      setFetchedCart(fetchedCart);
+    };
+    fetchCart();
+  }, []);
+
   return (
     <div className={styles.container}>
+      {/* <h1>Your Cart ({itemsAmount} items)</h1> */}
       <h1>Your Cart</h1>
-      <CartDetail
-        cart={cart}
-        handleChangeQty={handleChangeQty}
-        handleCheckout={handleCheckout}
-      />
+      {cart || fetchedCart ? (
+        <div className={styles.container}>
+          <CartDetail
+            cart={cart}
+            handleChangeQty={handleChangeQty}
+            fetchedCart={fetchedCart}
+            setFetchedCart={setFetchedCart}
+            handleAddToWishList={handleAddToWishList}
+          />
+          <div className={styles.total}>TOTAL: ${fetchedCart.orderTotal.toFixed(2)}</div>
+          <br />
+          <button onClick={handleCheckout} className={styles.checkoutBtn}>
+            CHECKOUT
+          </button>
+        </div>
+      ) : (
+        <p>YOUR CART IS EMPTY.</p>
+      )}
     </div>
   );
 }

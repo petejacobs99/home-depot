b6a50690796e26c0368805dd8de69f94e46e8e17
username <Jzazzi56@gmail.comn>
2023-09-13 17:28:59 -0700

    seed and order model fixes

--- models/order.js
+++ models/order.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const itemSchema = require('./itemSchema')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const itemSchema = require('./itemSchema');
 
 const lineItemSchema = new Schema(
 	{
 		qty: { type: Number, default: 1 },
-		item: itemSchema,
+		item: itemSchema
 	},
 	{
 		timeStamps: true,
 		toJSON: { virtuals: true }
 	}
-)
+);
 
 lineItemSchema.virtual('extPrice').get(function () {
-	return this.qty * this.item.price
-})
+	return this.qty * this.item.price;
+});
 
 const orderSchema = new Schema(
 	{
@@ -27,53 +27,56 @@ const orderSchema = new Schema(
 		timeStamps: true,
 		toJSON: { virtuals: true }
 	}
-)
+);
 
 orderSchema.virtual('orderTotal').get(function () {
-	return this.lineItems.reduce((total, item) => total + item.extPrice, 0)
-})
+	return this.lineItems.reduce((total, item) => total + item.extPrice, 0);
+});
 
 orderSchema.virtual('totalQty').get(function () {
-	return this.lineItems.reduce((total, item) => total + item.qty, 0)
-})
+	return this.lineItems.reduce((total, item) => total + item.qty, 0);
+});
 
 orderSchema.virtual('orderId').get(function () {
-	return this.id.slice(-6).toUpperCase()
-})
+	return this.id.slice(-6).toUpperCase();
+});
 
-orderSchema.static.getCart = function (userId) {
+orderSchema.statics.getCart = function (userId) {
 	return this.findOneAndUpdate(
+		// query
 		{ user: userId, isPaid: false },
+		// update
 		{ user: userId },
+		// upsert option will create the doc if it doesn't exist
 		{ upsert: true, new: true }
-	)
-}
+	);
+};
 
 orderSchema.methods.addItemToCart = async function (itemId) {
-	const cart = this
+	const cart = this;
 	const lineItem = cart.lineItems.find((lineItem) =>
 		lineItem.item._id.equals(itemId)
-	)
+	);
 	if (lineItem) {
-		lineItem.qty += 1
+		lineItem.qty += 1;
 	} else {
-		const item = await mongoose.model('Item').findById(itemId)
-		cart.lineItems.push({ item: item })
+		const item = await mongoose.model('Item').findById(itemId);
+		cart.lineItems.push({ item: item });
 	}
-	return cart.save()
-}
+	return cart.save();
+};
 
 orderSchema.methods.setItemQty = function (itemId, newQty) {
-	const cart = this
+	const cart = this;
 	const lineItem = cart.lineItems.find((lineItem) =>
 		lineItem.item._id.equals(itemId)
-	)
+	);
 	if (lineItem && newQty <= 0) {
-		lineItem.deleteOne()
+		lineItem.deleteOne();
 	} else if (lineItem) {
-		lineItem.qty = newQty
+		lineItem.qty = newQty;
 	}
-	return cart.save()
-}
+	return cart.save();
+};
 
-module.exports = mongoose.model('order', orderSchema)
+module.exports = mongoose.model('Order', orderSchema);

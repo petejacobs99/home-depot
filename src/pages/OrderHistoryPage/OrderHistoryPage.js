b970ed952501aa105efce467595a2ff9a955ebc8
misselysea <elyseacrawford@gmail.com>
2023-09-14 21:25:13 -0400

    order history

--- src/pages/OrderHistoryPage/OrderHistoryPage.js
+++ src/pages/OrderHistoryPage/OrderHistoryPage.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import styles from './OrderHistoryPage.module.scss';
-import OrderList from '../../components/OrderList/OrderList'
+import { getOrderHistory } from '../../utilities/orders-api';
+import OrderList from '../../components/OrderList/OrderList';
 
-export default function OrderHistoryPage({ user }) {
+export default function OrderHistoryPage() {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     const fetchOrderHistory = async () => {
       try {
-        const response = await fetch(`/api/orders/${user.id}`);
-        const data = await response.json();
-        setOrders(data.orders);
+        const response = await getOrderHistory(); 
+        console.log(response);
+        if (response.status === 200) {
+          const data = await response.json();
+          setOrders(data);
+        } else {
+          throw new Error(`Error fetching order history - Status ${response.status}`);
+        }
       } catch (error) {
         console.error('Error fetching order history:', error);
       }
     };
 
     fetchOrderHistory();
-  }, [user]);
+  }, []);
 
   return (
     <div className={styles.container}>
